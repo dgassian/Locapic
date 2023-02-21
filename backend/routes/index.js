85b9e9fa75ff1948117cfcad51564f942cabb2fc
damien gassian <damien.gassian@orange.fr>
2023-02-21 19:04:12 +0100

    save project for gitHub

---
+++ backend/routes/index.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var router = express.Router();
+require("../models/connection");
+
+const Place = require("../models/places");
+const { checkBody } = require("../modules/checkBody");
+
+router.post("/places", (req, res) => {
+	if (!checkBody(req.body, ["nickname", "name", "latitude", "longitude"])) {
+		res.json({ result: false, error: "Missing or empty fields" });
+		return;
+	}
+
+	const { nickname, name, latitude, longitude } = req.body;
+	const newPlace = new Place({
+		nickname,
+		name,
+		latitude,
+		longitude,
+	});
+
+	newPlace.save().then(() => {
+		res.json({ result: true });
+	});
+});
+
+router.get("/places/:nickname", (req, res) => {
+	Place.find({
+		nickname: { $regex: new RegExp(req.params.nickname, "i") },
+	}).then((data) => {
+		res.json({ result: true, places: data });
+	});
+});
+
+router.delete("/places", (req, res) => {
+	if (!checkBody(req.body, ["nickname", "name"])) {
+		res.json({ result: false, error: "Missing or empty fields" });
+		return;
+	}
+
+	const { nickname, name } = req.body;
+
+	Place.deleteOne({
+		nickname: { $regex: new RegExp(nickname, "i") },
+		name: { $regex: new RegExp(name, "i") },
+	}).then((deletedDoc) => {
+		if (deletedDoc.deletedCount > 0) {
+			res.json({ result: true });
+		} else {
+			res.json({ result: false, error: "Places not found" });
+		}
+	});
+});
+
+module.exports = router;
